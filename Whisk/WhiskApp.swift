851893b9b2e0e36b2603312f40e7ee96f0ff5388
duynguyen-workspace <duynguyen.workspace@gmail.com>
2023-07-31 14:36:05 +0700

    Initial Commit

---
+++ Whisk/WhiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiskApp.swift
+//  Whisk
+//
+//  Created by Nguyễn Anh Duy on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
